8df31b114b7cfc291e0747d054ad04bc4f4f0a6e
unknown <kaizerdavid.palabay@lorma.edu>
2026-01-28 11:31:01 +0800

    gonna clean workspace

--- Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-
-    public static void main(String[] args) {
-        Circle circle = new Circle(250);
-
-        System.out.println("Area of the circle: " + circle.getArea());
-    }
-}
---
+++ practice/week3/Shop.java
@@ -0,0 +1,9 @@
+package practice.week3;
+
+import java.util.ArrayList;
+
+public class Shop {
+    String name;
+    Product[] productList;
+    ArrayList<String> arrProducts = new ArrayList<>();
+}
